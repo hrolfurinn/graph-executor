ee56906a559021fd0ef58272e49ce1c598ced2c1
hrolfurinn <106666932+hrolfurinn@users.noreply.github.com>
2024-02-09 13:22:36 -0500

    Add operator tests

--- src/operator.rs
+++ src/operator.rs
@@ -10,7 +10,7 @@ pub trait Operator: Debug {
     ///
     /// * `input_buffers` - A slice of `Buffer` instances containing the inputs to the operation.
     /// * `output_buffers` - A mutable slice of `Buffer` instances where the operation's results should be stored.
-    fn execute(&self, input_buffers: &[Buffer], output_buffers: &mut [Buffer]);
+    fn execute(&self, input_buffers: &Vec<Buffer>, output_buffers: &mut Vec<Buffer>);
 
     /// Returns the expected number of input buffers for this operation.
     ///
@@ -38,3 +38,49 @@ pub trait Operator: Debug {
     fn compute_output_buffer_sizes(&self, input_buffer_sizes: &[usize]) -> Vec<usize>;
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::buffer::Buffer;
+
+    #[derive(Debug, Clone)]
+    struct TestOperator;
+
+    impl Operator for TestOperator {
+        fn execute(&self, input_buffers: &Vec<Buffer>, output_buffers: &mut Vec<Buffer>) {
+            // TODO: Add check for input and output buffer sizes
+            for (input, output) in input_buffers.iter().zip(output_buffers.iter_mut()) {
+                output.write(input.read());
+            }
+        }
+
+        fn input_count(&self) -> usize {
+            2
+        }
+
+        fn output_count(&self) -> usize {
+            2
+        }
+
+        fn compute_output_buffer_sizes(&self, input_buffer_sizes: &[usize]) -> Vec<usize> {
+            input_buffer_sizes.to_vec()
+        }
+    }
+
+    #[test]
+    fn test_operator_execute() {
+        let operator = TestOperator;
+        let input_buffers = vec![Buffer::new_from_data(vec![1 as u8; 10]), Buffer::new_from_data(vec![2 as u8; 20])];
+        let mut output_buffers = vec![Buffer::new_from_size(10), Buffer::new_from_size(20)];
+        operator.execute(&input_buffers, &mut output_buffers);
+        assert_eq!(input_buffers, output_buffers);
+    }
+
+    #[test]
+    fn test_operator_compute_output_buffer_sizes() {
+        let operator = TestOperator;
+        let input_buffer_sizes = vec![10, 20];
+        let expected_output_buffer_sizes = vec![10, 20];
+        assert_eq!(operator.compute_output_buffer_sizes(&input_buffer_sizes), expected_output_buffer_sizes);
+    }
+}
